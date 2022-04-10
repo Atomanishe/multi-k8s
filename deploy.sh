a561361367edca49655f87bceb268317be713ec8
Atomanishe <wordkeeper@mail.ru>
2022-04-10 22:55:41 +0300

    Final7

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t atomanishe/multi-client-k8s:latest -t atomanishe/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t atomanishe/multi-server-k8s-pgfix:latest -t atomanishe/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t atomanishe/multi-worker-k8s:latest -t atomanishe/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t atomanishe/multi-client:latest -t atomanishe/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t atomanishe/multi-server:latest -t atomanishe/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t atomansihe/multi-worker:latest -t atomanishe/multi-worker:$SHA-f ./worker/Dockerfile ./worker
 
-docker push atomanishe/multi-client-k8s:latest
-docker push atomanishe/multi-server-k8s-pgfix:latest
-docker push atomanishe/multi-worker-k8s:latest
+docker push atomansihe/multi-client:latest
+docker push atomansihe/multi-server:latest
+docker push atomanishe/multi-worker:latest
 
-docker push atomanishe/multi-client-k8s:$SHA
-docker push atomanishe/multi-server-k8s-pgfix:$SHA
-docker push atomanishe/multi-worker-k8s:$SHA
+docker push atomansihe/multi-client:$SHA
+docker push atomansihe/multi-server:$SHA
+docker push atomanishe/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=atomanishe/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=atomanishe/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=atomanishe/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployments server=atomanishe/multi-server:$SHA
+kubectl set image deployments/client-deployments client=atomanishe/multi-client:$SHA
+kubectl set image deployments/worker-deployments worker=atomanishe/multi-worker:$SHA
